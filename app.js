4a881f624c48599ea7b87c9f6d5019cd2e12bfcb
Shai Gerbi <ShaiGerbi@gmail.com>
2019-10-20 16:49:17 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,34 @@
+const app = new Vue(
+{
+    el: '#app',
+    data: {
+        dog: './DogProfile.png'
+    },
+
+    methods: {
+        loadDog: function () {
+            fetch('https://random.dog/woof.json?include=jpg,png,gif,jpeg')
+                .then((response) =>
+                    {
+                        if (response.status !== 200)
+                        {
+                            console.log(`Looks like there was a problem. Status Code: ${response.status}`);
+                            return;
+                        }
+
+                        response.json().then((data) =>
+                        {
+                            app.dog = data.url;
+                        });
+                    }
+                )
+                .catch(function(error)
+                {
+                    console.log('Fetch Error :-S', error);
+                });
+        }
+    }
+
+});
+
+app.loadDog();
