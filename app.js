427610df524523c618d767dcbcecfe0d830044eb
Shai Gerbi <ShaiGerbi@gmail.com>
2020-01-28 17:08:27 +0200

    Using a new writing method

--- app.js
+++ app.js
@@ -2,28 +2,25 @@ const app = new Vue(
 {
     el: '#app',
     data: {
-        dog: './DogProfile.png'
+        dog: ['./DogProfile.png',],
     },
 
     methods: {
-        loadDog: function () {
+        loadDog () {
             fetch('https://random.dog/woof.json?include=jpg,png,gif,jpeg')
-                .then((response) =>
-                    {
-                        if (response.status !== 200)
-                        {
+                .then((response) => {
+                        if (response.status !== 200) {
                             console.log(`Looks like there was a problem. Status Code: ${response.status}`);
                             return;
                         }
 
-                        response.json().then((data) =>
-                        {
-                            app.dog = data.url;
+                        response.json()
+                                .then((data) => {
+                                app.dog = data.url;
                         });
                     }
                 )
-                .catch(function(error)
-                {
+                .catch(error => {
                     console.log('Fetch Error :-S', error);
                 });
         }
